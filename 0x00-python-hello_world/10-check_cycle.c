b846c6ece66bdabc6c21586dd61fab6f1819457a
Naan Mohammed <naanahmed.na@gmail.com>
2022-08-22 20:22:14 +0300

    'New'

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,69 +1,36 @@
-#include "lists.h"
+/*
+ * File: 10-check_cycle.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdlib.h>
+#include "lists.h"
 
 /**
- * _realloc - Reallocates a memory block
- * @ptr: The pointer to the previous memory block
- * @old_size: The size of the old memory block
- * @new_size: The size of the new memory block
+ * check_cycle - Checks if a singly-linked list contains a cycle.
+ * @list: A singly-linked list.
  *
- * Return: The pointer to the new memory block otherwise NULL
+ * Return: If there is no cycle - 0.
+ *         If there is a cycle - 1.
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-{
-	void *new_ptr;
-	unsigned int min_size = old_size < new_size ? old_size : new_size;
-	unsigned int i;
-
-	if (new_size == old_size)
-		return (ptr);
-	if (ptr != NULL)
-	{
-		if (new_size == 0)
-		{
-			free(ptr);
-			return (NULL);
-		}
-		new_ptr = malloc(new_size);
-		if (new_ptr != NULL)
-		{
-			for (i = 0; i < min_size; i++)
-				*((char *)new_ptr + i) = *((char *)ptr + i);
-			free(ptr);
-			return (new_ptr);
-		}
-		free(ptr);
-		return (NULL);
-	}
-	else
-	{
-		new_ptr = malloc(new_size);
-		return (new_ptr);
-	}
-}
-
-
-
-/**
- * check_cycle - checks if a singly linked list has a cycle in it
- * @list: list head
- * Return: 0 if no, 1 if yes
- */
-
 int check_cycle(listint_t *list)
 {
-	listint_t *fast, *slow = list;
+	listint_t *turtle, *hare;
 
-	if (list == NULL)
+	if (list == NULL || list->next == NULL)
 		return (0);
 
-	fast = list->next;
-	while (slow != NULL && fast != NULL && fast->next != NULL)
+	turtle = list->next;
+	hare = list->next->next;
+
+	while (turtle && hare && hare->next)
 	{
-		if (slow == fast)
+		if (turtle == hare)
 			return (1);
-		fast = fast->next->next;
-		slow = slow->next;
+
+		turtle = turtle->next;
+		hare = hare->next->next;
 	}
+
 	return (0);
 }
