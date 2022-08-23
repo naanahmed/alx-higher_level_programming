6466d9d542e010e6eaa74f32b4330a090768cfe0
Naan Mohammed <naanahmed.na@gmail.com>
2022-08-23 16:55:45 +0300

    Advanced Tasks 1 Edit 4- Complete Revamp 1

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-for i in range(0, 26, 2):
-    print("{:c}{:c}".format(122 - i, (122 - i - 1) - 32), end='')
+i = 122
+while i >= 97:
+    flag = 0
+    if i % 2 != 0:
+        i = i - 32
+        flag = 1
+    print("{:s}".format(chr(i)), end="")
+    if flag == 1:
+        i = i + 32
+    i = i - 1
