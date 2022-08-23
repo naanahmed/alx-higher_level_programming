506330121845ddbd9531f0b66a22a237466ca892
Naan Mohammed <naanahmed.na@gmail.com>
2022-08-23 16:46:19 +0300

    Advanced Tasks 1 Edit 2

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,7 @@
 for i in reversed(range(97, 123)):
     if (i % 2 == 0):
         print('{:c}'.format(i), end='')
-    elif (i==122)
+    elif (i==122):
         print("\n")
     else:
         print('{:c}'.format(i - 32), end='')
