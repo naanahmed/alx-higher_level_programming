4620e96e86d52dac6a5f57d7eedca22d38c08b34
Naan Mohammed <naanahmed.na@gmail.com>
2022-09-13 21:43:18 +0300

    new adv tasks  1

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,42 +1,11 @@
-asses for a singly-linked list."""
-
-
-class Node:
-    """Represent a node in a singly-linked list."""
-
-    def __init__(self, data, next_node=None):
-        """Initialize a new Node.
-        Args:
-            data (int): The data of the new Node.
-            next_node (Node): The next node of the new Node.
-        """
-        self.data = data
-        self.next_node = next_node
-
-    @property
-    def data(self):
-        """Get/set the data of the Node."""
-        return (self.__data)
-
-    @data.setter
-    def data(self, value):
-        if not isinstance(value, int):
-            raise TypeError("data must be an integer")
-        self.__data = value
-
-    @property
-    def next_node(self):
-        """Get/set the next_node of the Node."""
 #!/usr/bin/python3
 
-"""Define classes for a singly-linked list."""
-
-
 class Node:
     """Represent a node in a singly-linked list."""
 
     def __init__(self, data, next_node=None):
         """Initialize a new Node.
+
         Args:
             data (int): The data of the new Node.
             next_node (Node): The next node of the new Node.
@@ -76,8 +45,10 @@ class SinglyLinkedList:
 
     def sorted_insert(self, value):
         """Insert a new Node to the SinglyLinkedList.
+
         The node is inserted into the list at the correct
         ordered numerical position.
+
         Args:
             value (Node): The new Node to insert.
         """
