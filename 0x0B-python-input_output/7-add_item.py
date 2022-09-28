7f4df13cb8e5b4fc4029b528aca1e5d0ec7ab3b3
Naan Mohammed <naanahmed.na@gmail.com>
2022-09-29 00:34:12 +0300

    task 7 and adv tasks

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-'''
-Module that adds args to JSON file
-'''
+from os import path
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-import sys
-import os
+if path.exists('add_item.json'):
+    obj_json_file = load_from_json_file('add_item.json')
+else:
+    obj_json_file = []
 
-arg_list = sys.argv[1:]
+for i in range(1, len(argv)):
+    obj_json_file.append(argv[i])
 
-save_JSON = __import__('5-save_to_json_file').save_to_json_file
-load_JSON = __import__('6-load_from_json_file').load_from_json_file
-
-lisst = []
-if os.path.exists('add_item.json'):
-    lisst = load_JSON('add_item.json')
-
-save_JSON(lisst + arg_list, "add_item.json")
+save_to_json_file(obj_json_file, 'add_item.json')
