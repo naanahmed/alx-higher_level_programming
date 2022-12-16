0b5b4b39b850228235b410b5eefd267126026a12
Naan Ahmed <naanahmed.na@gmail.com>
2022-12-17 00:56:26 +0300

    edit task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-"""Fetches the URL: https://intranet.hbtn.io/status
-"""
+'''
+script that fetches https://intranet.hbtn.io/status
+'''
+import urllib.request as req
 
-from urllib.request import Request, urlopen
-
-
-if __name__ == "__main__":
-    req = Request('https://intranet.hbtn.io/status')
-
-    with urlopen(req) as res:
+if __name__ == '__main__':
+    with req.urlopen('https://intranet.hbtn.io/status') as res:
         content = res.read()
-        utf8_content = content.decode('utf-8')
-
-        print('Body response:')
-        print('\t- type: {_type}'.format(_type=type(content)))
-        print('\t- content: {_content}'.format(_content=content))
-        print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=utf8_content))
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
